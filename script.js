8f70793258025d1ed6d96af3b600456196263dc7
Samuel Alcosser <samalcosser@gmail.com>
2020-07-22 15:35:17 -0400

    clean up

---
+++ script.js
@@ -0,0 +1,8 @@
+function showCode(){
+    var c = document.getElementById("csc");
+    if(c.style.display === "none"){
+        c.style.display = "block";
+    }else{
+        c.style.display = "none";
+    }
+}
