c0831b578c3ec9700b6e439b4bde164f5b7126dc
Samuel Alcosser <samalcosser@gmail.com>
2020-12-20 21:21:32 -0500

    First commit

---
+++ DSA.php
@@ -0,0 +1,11 @@
+<?php require("header.php");?>
+
+
+
+
+<!-- Content!! -->
+
+
+<?php require("footer.php");?>
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,111 +1,113 @@
 <?php
-if(session_id() == '' || !isset($_SESSION)) {
+if (session_id() == '' || !isset($_SESSION)) {
 
-    session_start();
-    $_SESSION['ran'] = 1;
+  session_start();
+  $_SESSION['ran'] = 1;
 }
 
 ?>
 
 <!DOCTYPE html>
 
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Sam Alcosser's Portfolio</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-  
-    <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Sam Alcosser's Portfolio</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 
-<!-- Popper JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <!-- jQuery library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-<link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@500&display=swap" rel="stylesheet">
+  <!-- Popper JS -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-<script src="script.js"></script>
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet"> 
-<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300&display=swap" rel="stylesheet"> 
-<link rel="stylesheet" href="main.css">
-<link rel="apple-touch-icon" sizes="57x57" href="img/favicons/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="img/favicons/apple-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="img/favicons/apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="img/favicons/apple-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="img/favicons/apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="img/favicons/apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="img/favicons/apple-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="img/favicons/apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="img/favicons/apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="img/favicons/android-icon-192x192.png">
-<link rel="icon" type="image/png" sizes="32x32" href="img/favicons/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="img/favicons/favicon-96x96.png">
-<link rel="icon" type="image/png" sizes="16x16" href="img/favicons/favicon-16x16.png">
-<link rel="manifest" href="img/favicons/manifest.json">
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="img/favicons/ms-icon-144x144.png">
-<meta name="theme-color" content="#ffffff">
-<link rel="stylesheet" href="prism.css">
-<script src="prism.js"></script>
+  <!-- Latest compiled JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@500&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+  <script src="script.js"></script>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="main.css">
+  <link rel="apple-touch-icon" sizes="57x57" href="img/favicons/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="img/favicons/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="img/favicons/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="img/favicons/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="img/favicons/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="img/favicons/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="img/favicons/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="img/favicons/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="img/favicons/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="img/favicons/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="img/favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="img/favicons/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="img/favicons/favicon-16x16.png">
+  <link rel="manifest" href="img/favicons/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="img/favicons/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+  <link rel="stylesheet" href="prism.css">
+  <script src="prism.js"></script>
 </head>
 
 <body onload="javascript:sizing(),setMode()">
- <?php session_start();?>
+  <?php session_start(); ?>
 
-</div>
-<div class="pos-f-t">
-  <div class="collapse" id="navbarToggleExternalContent">
-    <div class="bg-dark p-4">
-     
-    <nav class="nav navbar-expand-sm bg-dark navbar-dark nbar">
-  <ul>
-  <li class="nav-item">
-    <a class="nav-link active" href="index.php" tabindex="-1" aria-disabled="true"><i class="fa fa-home" aria-hidden="true"></i>
-</a>
-  </li>
-      <li class="nav-item">
-
-          <a class="nav-link active" href="MIPC.php">MIP Connector</a>
-      </li>
-
-  <li class="nav-item">
-    
-       <a class="nav-link active" href="WorkoutTracker.php">Workout Tracker</a>
-  </li>
-
-  <li class="nav-item">
-    
-       <a class="nav-link active" href="teamrcf.php">Team RCF Website (Not Live)</a>
-  </li>
-
-
-
-
-
-  </ul>
- 
-</nav>
-
-    </div>
   </div>
-  <nav class="navbar navbar-dark bg-dark">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon">   
-    <i class="fas fa-bars"></i>
-</span>
-    </button>
-   <button id="toggle-btn" class="btn" onclick="toggleMode()">Toggle Light / Dark Mode</button>
-  </nav>
-</div>
+  <div class="pos-f-t">
+    <div class="collapse" id="navbarToggleExternalContent">
+      <div class="bg-dark p-4">
+
+      <nav class="nav navbar-expand-sm bg-dark navbar-dark nbar">
+                        <ul>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="index.php" tabindex="-1" aria-disabled="true"><i class="fa fa-home" aria-hidden="true"></i>
+                                </a>
+                            </li>
 
 
 
-  
-<div class="container-fluid" style="margin-top: 54px">
-    
+                            <li class="nav-item">
+
+                                <a class="nav-link active" href="MIPC.php">MIP Connector</a>
+                            </li>
+
+                            <li class="nav-item">
+
+                                <a class="nav-link active" href="WorkoutTracker.php">Workout Tracker</a>
+                            </li>
+
+                            <li class="nav-item">
+
+                                <a class="nav-link active" href="teamrcf.php">Team RCF Website | (Not Live)</a>
+                            </li>
+                            <li class="nav-item">
+
+                                <a class="nav-link active" href="DSA.php">DSA Work | New!</a>
+                            </li>
+
+                        </ul>
+
+                    </nav>
+
+      </div>
+    </div>
+    <nav class="navbar navbar-dark bg-dark">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon">
+          <i class="fas fa-bars"></i>
+        </span>
+      </button>
+      <button id="toggle-btn" class="btn" onclick="toggleMode()">Toggle Light / Dark Mode</button>
+    </nav>
+  </div>
+
+
+
+
+  <div class="container-fluid" style="margin-top: 54px">
